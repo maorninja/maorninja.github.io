b84526e5934fe8ec7ee1d84596bb1cb658bc1b13
maorninja <maorninja@outlook.com>
2016-10-09 13:52:15 -0700

    Create index.php
    Testing

---
+++ index.php
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+    if (navigator.userAgent.indexOf('Nintendo 3DS') == 1) { //If the UserAgent is not "Nintendo 3DS"
+        location.replace('http://3dshacks.xyz'); //Redirect to an other page
+    }
+</script>If you see this, you're on a PC
