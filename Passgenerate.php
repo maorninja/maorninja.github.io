9eb2d72ec01bd17bbc43975946e7e4ccfa98e149
maorninja <maorninja@outlook.com>
2017-01-08 08:10:19 -0800

    Update Passgenerate.php
    Fixing the tags for the code.

--- Passgenerate.php
+++ Passgenerate.php
@@ -1,19 +1,16 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head><title>Password Generator</title></head>
-<script type="text/javascript" src="generator.js"></script> 
+<!DOCTYPE html>
+<html>
+<head><title>Password Generator</title>
+<script type="text/javascript" src="generator.js"></script></head>
+<body>
 <font color="#FF0000" size="5">Random password generator</font><br>A simple JavaScript who lets you generate a random characters sequence (uppercase, lowercase and numbers). A strong password is usually up to 8 characters<br>
 It looks a bit ugly right now, it's a WIP project. Have fun!
 <br><br>
-<body>
 
     Your password will appear here: <input type="text" name="password" id="password" value="">
     <br /><br />
 
 <a href="javascript:void(0)" onClick="create_password();""><button>Generate Password</button></a>
 
-</body>
 <br><br>
-By Super-toad 65</html>
+By Super-toad 65</body></html>
